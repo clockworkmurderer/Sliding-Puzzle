6855b484a97ee9e76490d091997107a386497362
clockworkmurderer <clockworkmurderer@clock>
2019-02-24 10:50:04 +0100

    move counter finished

--- ProgrammingProject/src/programming/project/PuzzleController.java
+++ ProgrammingProject/src/programming/project/PuzzleController.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.Map;
 import acm.graphics.GCompound;
+import acm.graphics.GLabel;
 import acm.graphics.GPoint;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
@@ -17,6 +18,9 @@ public class PuzzleController extends GraphicsProgram {
 
 	private static final int CELL_SIZE = 80;
 	private static final int GAP = 8;
+
+	private int moveCounter = 0;
+	private GLabel numberOfMoves;
 
 	private PuzzleModel board = new PuzzleModel();
 	private PuzzleView view = new PuzzleView();
@@ -32,7 +36,6 @@ public class PuzzleController extends GraphicsProgram {
 	@Override
 	public void init() {
 		super.init();
-		setSize(CELL_SIZE * 4 + 10, CELL_SIZE * 6 + 10);
 		addMouseListeners();
 		addKeyListeners();
 	}
@@ -51,6 +54,7 @@ public class PuzzleController extends GraphicsProgram {
 		view.draw(board.bottomLeft, inactivePieceColor);
 		view.draw(board.bottomRight, inactivePieceColor);
 		view.draw(board.middleSquare, inactivePieceColor);
+
 	}
 
 	@Override
@@ -58,14 +62,16 @@ public class PuzzleController extends GraphicsProgram {
 		activePiece = null;
 		repaintBoard();
 		lastClick = new GPoint(e.getPoint());
-		
+
 		/**
 		 * The next line reduces the clicked location to a fraction of the CELL_SIZE in
 		 * order to conform to the coordinate system.
 		 */
 		lastClick.setLocation((int) lastClick.getX() / CELL_SIZE, (int) lastClick.getY() / CELL_SIZE);
 
-		/** Checks against the HashMap to see if the clicked cell is occupied by a piece. */
+		/**
+		 * Checks against the HashMap to see if the clicked cell is occupied by a piece.
+		 */
 		for (Map.Entry<String, GPoint[]> entry : board.getPiecePositions().entrySet()) {
 			GPoint[] temp = entry.getValue();
 			for (int i = 0; i < temp.length; i++) {
@@ -75,6 +81,10 @@ public class PuzzleController extends GraphicsProgram {
 			}
 		}
 
+		/**
+		 * Uses the key from the HashMap to figure out which, if any, piece was clicked
+		 * and then sets the active piece to the piece which was clicked.
+		 */
 		if (name != null) {
 			switch (name) {
 			case "topLeft":
@@ -103,6 +113,7 @@ public class PuzzleController extends GraphicsProgram {
 		}
 	}
 
+	@Override
 	public void keyTyped(KeyEvent e) {
 		if (activePiece != null) {
 			switch (Character.toUpperCase(e.getKeyChar())) {
@@ -124,6 +135,11 @@ public class PuzzleController extends GraphicsProgram {
 		}
 	}
 
+	/**
+	 * This method determines the destination cells of the given move for the given
+	 * piece and passes that information to the Model in order to check if the move
+	 * being made is valid.
+	 */
 	private void movement(int x, int y) {
 		GPoint[] destination = new GPoint[activePiece.getCoordinates().length];
 		for (int i = 0; i < destination.length; i++) {
@@ -131,9 +147,11 @@ public class PuzzleController extends GraphicsProgram {
 					activePiece.getCoordinates()[i].getY() + y);
 		}
 
+		// if the move is not valid, do nothing. otherwise, make the move
 		if (!board.isMoveValid(new GamePiece(destination, activePiece.getName()))) {
 			return;
 		} else {
+			moveCounter++;
 			activePiece.movePiece(x, y);
 			board.setPiecePositions(activePiece.getName(), activePiece.getCoordinates());
 			repaintBoard();
@@ -141,7 +159,16 @@ public class PuzzleController extends GraphicsProgram {
 		}
 	}
 
+	/**
+	 * The PuzzleView is responsible for showing the user what is happening. It
+	 * accomplishes this by providing the draw method and the graphical version of
+	 * the game board and pieces.
+	 */
 	class PuzzleView {
+		/**
+		 * This method takes a GamePiece as an argument and draws it with the specified
+		 * color.
+		 */
 		public void draw(GamePiece piece, Color c) {
 
 			if (piece.equals(board.topLeft)) {
@@ -158,6 +185,7 @@ public class PuzzleController extends GraphicsProgram {
 			}
 		}
 
+		/** This method sets up the game board and the move counter label. */
 		private void gridSetup() {
 			for (int i = 0; i < 4; i++) {
 				for (int j = 0; j < 6; j++) {
@@ -165,8 +193,12 @@ public class PuzzleController extends GraphicsProgram {
 					add(cell);
 				}
 			}
+			setSize(CELL_SIZE * 4 + 10, CELL_SIZE * 7 + 10);
+			numberOfMoves = new GLabel("Number of Moves: " + moveCounter, CELL_SIZE, CELL_SIZE * 6 + 25);
+			add(numberOfMoves);
 		}
 
+		/** Draws the top left piece. */
 		private GCompound drawTopLeft(Color c) {
 
 			topLeft = new GCompound();
@@ -184,6 +216,7 @@ public class PuzzleController extends GraphicsProgram {
 			return topLeft;
 		}
 
+		/** Draws the top right piece. */
 		private GCompound drawTopRight(Color c) {
 
 			topRight = new GCompound();
@@ -201,6 +234,7 @@ public class PuzzleController extends GraphicsProgram {
 			return topRight;
 		}
 
+		/** Draws the bottom left piece. */
 		private GCompound drawBottomLeft(Color c) {
 
 			bottomLeft = new GCompound();
@@ -218,6 +252,7 @@ public class PuzzleController extends GraphicsProgram {
 			return bottomLeft;
 		}
 
+		/** Draws the bottom right piece. */
 		private GCompound drawBottomRight(Color c) {
 
 			bottomLeft = new GCompound();
@@ -235,6 +270,7 @@ public class PuzzleController extends GraphicsProgram {
 			return bottomLeft;
 		}
 
+		/** Draws the middle piece. */
 		private GCompound drawMiddleSquare(Color c) {
 
 			middleSquare = new GCompound();
